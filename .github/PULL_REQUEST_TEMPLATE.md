06acc9c2ec2470a158d162d4bb0351741fec36ab
Christopher Milan <chris.m.milan@gmail.com>
2019-10-30 12:53:34 -0400

    pending changes

--- .github/PULL_REQUEST_TEMPLATE.md
+++
@@ -1,4 +0,0 @@
-- [ ] I have added the new command module (in the `src/commands/` directory)
-- [ ] I have enabled the new command (in `src/commands/index.js`)
-- [ ] I have linted the code, and resolved all errors (`npm run lint`)
-- [ ] I have read the [contributing guidelines](https://github.com/christopherm99/YeetBot2.0/blob/master/CONTRIBUTING.md).
--- src/commands/index.js
+++ src/commands/index.js
@@ -1,22 +1,6 @@
-import clean from "./clean.js";
 import overlay from "./overlay.js";
-import yeet from "./yeet.js";
 
 export default [
-  {
-    name: "clean",
-    minArgs: 1,
-    usage: `clean <IMAGE_URL>
-Finds highest confidence face in an image, and returns a cropped and cleaned version of the image.`,
-    execute: clean
-  },
-  {
-    name: "yeet",
-    minArgs: 2,
-    usage: `yeet <BACKDROP_URL> <OVERLAY_URL>
-Cleans overlay, finds all faces in backdrop image, and returns an image with faces overlaid.`,
-    execute: yeet
-  },
   {
     name: "overlay",
     minArgs: 2,
